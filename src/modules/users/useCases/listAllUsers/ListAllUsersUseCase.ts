eb37585536290e4ea1693253d878e1628fefdf58
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-23 18:33:19 -0300

    testing the use-cases

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User requesting the list doesn't exists.");
+    }
+
+    if (!user.admin) {
+      throw new Error("Unauthorized.");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
